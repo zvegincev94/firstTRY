a48b488e98b8e028c2f0bdc9d3105d73ebd31892
Zvegincev Dima NETBOOK <zvegincev94@gmail.com>
2015-09-16 13:52:31 +0300

    V_0.1

--- MMA_LB1/src/net/zvegincev/Main.java
+++ MMA_LB1/src/net/zvegincev/Main.java
@@ -1,18 +1,38 @@
 package net.zvegincev;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
 
-	int mas[][];
+	static int mas;
+	static int n;
 	
 	public Main(){}
 	
-	public void firstStep(){
+	public char[] firstStep(){
 		
+		return null;
+	}
+	
+	private int generateInt(int min, int max) {
+		// TODO Auto-generated method stub
+		Random rand = new Random();
+		int number = (int)(min + rand.nextDouble()*(max - min));
+		return number;
 	}
 	
 	public static void main(String[] args) {
+		//Scanner sc = new Scanner(System.in);
+		//System.out.print("¬ведите n");
+		//n = sc.nextInt();
+		Main main = new Main();
+		System.out.print(mas = main.generateInt(2, 4));
+			
 		// TODO Auto-generated method stub
 
 	}
+
+	
 
 }
