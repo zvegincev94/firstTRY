b57194bbf2f430f855e60601cd347bbb4b8d0385
Zvegincev Dmitry <zvegincev94@gmail.com>
2015-09-17 21:08:28 +0300

    MMA_LB1_V0.15

--- MMA_LB1/src/net/zvegincev/Main.java
+++ MMA_LB1/src/net/zvegincev/Main.java
@@ -4,35 +4,70 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
-
-	static int mas;
-	static int n;
+	
 	
 	public Main(){}
 	
-	public char[] firstStep(){
-		
-		return null;
-	}
-	
-	private int generateInt(int min, int max) {
-		// TODO Auto-generated method stub
-		Random rand = new Random();
-		int number = (int)(min + rand.nextDouble()*(max - min));
-		return number;
-	}
-	
 	public static void main(String[] args) {
-		//Scanner sc = new Scanner(System.in);
-		//System.out.print("Введите n");
-		//n = sc.nextInt();
-		Main main = new Main();
-		System.out.print(mas = main.generateInt(2, 4));
-			
 		// TODO Auto-generated method stub
-
+		
+		int n;
+		Scanner sc = new Scanner(System.in);
+		Main main = new Main();
+		Random rand = new Random();
+		System.out.print("Введите n ");
+		n = sc.nextInt();
+		
+		Object[] massivs = new Object[n];
+		int mas[][] = new int[n][n];
+		int mas1[][] = new int[n][n];
+		int[] rez = new int[n];
+		int[] min = new int[n];
+		
+		for(int i=0; i<n; i++){
+			for(int j=0; j<n; j++){
+				mas[i][j] = 10 + rand.nextInt(90);
+				System.out.print(mas[i][j] + " ");
+				mas1[i][j] = mas[i][j];
+			}
+			System.out.println(" ");
+		}//Генерирование значений
+		
+		while(true){
+			for(int i=0; i<n; i++){
+				min[i] = mas1[i][0];
+			}
+			for(int i=0; i<n; i++){
+				for(int j=0; j<n; j++){
+					if(mas[i][j]<min[i]){
+						min[i]=mas[i][j];
+					}
+				}
+			}//Шаг 1 -- Нахождение минимального в строках (+)
+			
+			System.out.println(" ");
+			
+		
+			for(int i=0; i<n; i++){
+				for(int j=0; j<n; j++){
+					mas1[i][j] = mas1[i][j] - min[i]; 
+					System.out.print(mas1[i][j] + " ");
+				}
+				System.out.println(" ");
+			}//шаг 1 -- вычитание (+)
+			
+			for(int i=0; i<n; i++){
+				for(int j=0; j<n; j++){
+					if(mas1[i][j] == 0){
+						rez[i]=mas[i][j];
+					}
+				}
+				System.out.print(rez[i] + " ");
+			}//шаг 2
+			break;
+		}
+		
+		
+		
 	}
-
-	
-
 }
